351bae48e1719234f05468f78cc718e9bbff22c2
mpiers110 <mpiers110@gmail.com>
2023-11-20 07:53:20 +0300

    change transaction schema

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -2,29 +2,17 @@ const mongoose = require('mongoose');
 
 const transactionSchema = mongoose.Schema(
   {
-    "password": {
-      type: String,
-      required: [true, `Driver's Password is required`]
-    },
-    "driverName": {
-      type: String,
-      required: [true, `Driver's name is required`]
-    },
-    "driverContact": {
+    "phoneNumber": {
       type: Number,
-      required: [true, `Driver's phone number is required`]
+      required: [true, `Phone number is required`]
     },
-    "driverNationalID": {
+    "transactionID": {
+      type: String,
+      required: [true, `TransactionID is required`]
+    },
+    "amount": {
       type: Number,
-      required: [true, `Driver's ID number is required`]
-    },
-    "assignedGroup": {
-      type: String,
-      required: [true, `Driver's assigned group is required`]
-    },
-    "drivingLicense": {
-      type: String,
-      required: [true, `Driver's license is required`]
+      required: [true, `Amount is required`]
     }
   },
   {
@@ -35,13 +23,4 @@ const transactionSchema = mongoose.Schema(
 const Transaction = mongoose.model('Transaction', transactionSchema);
 
 module.exports = Transaction;
-/*
-{
-  "password": "freddy",
-  "driverName": "Fredrick Isika",
-  "driverContact": 25412123456,
-  "driverNationalID": 39294057,
-  "assignedGroup": "Makini-A",
-  "drivingLicense": "https://firebase/hosting/driversLicenses/KenHa/300.jpg"
-}
-*/
+
